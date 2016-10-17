7498dd238c1e0791c97c1fcc7523d98d7cc049f7
Debopam Chanda <deepchanda27@gmail.com>
2016-10-17 06:53:41 +0200

    Add videojs-hotkeys plugin, fix poster not updating bug and add styles

--- src/app/youtubevideoplayer.js
+++ src/app/youtubevideoplayer.js
@@ -7,7 +7,12 @@ angular
       $scope.loadVideo = function () {
         videojs('vid1').ready(function() {
           var myPlayer = this;
+          myPlayer.hotkeys({
+            enableJogStyle: true,
+            seekStep: 0
+          });
           myPlayer.src({type: 'video/youtube', src: $scope.urlText});
+          myPlayer.poster('');
         });
       };
     }
