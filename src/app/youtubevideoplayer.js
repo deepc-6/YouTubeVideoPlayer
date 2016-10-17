ac76b09316c2fb6f966e12df32619261c89b4f70
Debopam Chanda <deepchanda27@gmail.com>
2016-10-17 08:31:34 +0200

    Add unit test and document code

--- src/app/youtubevideoplayer.js
+++ src/app/youtubevideoplayer.js
@@ -3,17 +3,49 @@ angular
   .component('app', {
     templateUrl: 'app/youtubevideoplayer.html',
     controller: function videoController ($scope) {
+
+      /**
+       * @property {string} urlText - The input for youtube url.
+       */
       $scope.urlText = '';
+
+      /**
+       *  @function loadVideo - This function initializes the hotkeys and sets the source for the video from the input field.
+       */
       $scope.loadVideo = function () {
+
+        /**
+         * @function ready - This function triggers any code that requires the player's API.
+         */
         videojs('vid1').ready(function() {
+
+          /**
+           * Create an instance of the current player.
+           */
           var myPlayer = this;
+
+          /**
+           * Configure hotkeys for the player.
+           */
           myPlayer.hotkeys({
             enableJogStyle: true,
             seekStep: 0
           });
+
+          /**
+           * Set the source for the player.
+           */
           myPlayer.src({type: 'video/youtube', src: $scope.urlText});
+
+          /**
+           * Set poster to null for the player to automatically load it when source is changed.
+           */
           myPlayer.poster('');
+
         });
+
       };
+
     }
+
   });
---
+++ src/test/youtubevideoplayer.spec.js
@@ -0,0 +1,19 @@
+describe('component: app', function () {
+  beforeEach(module('app', function ($provide) {
+    $provide.factory('app', function () {
+      return {
+        templateUrl: 'app/hello.html'
+      };
+    });
+  }));
+  beforeEach(angular.mock.inject(function(_$componentController_) {
+    $componentController = _$componentController_;
+  }));
+  it('loadVideo function should be called', function () {
+    var $scope = {};
+    var controller = $componentController('app', { $scope: $scope });
+    spyOn($scope, 'loadVideo');
+    $scope.loadVideo();
+    expect($scope.loadVideo).toHaveBeenCalled();
+  });
+});
